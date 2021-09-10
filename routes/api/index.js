7586f3223c7d9472dabfc453ca8a455e5abb4efc
AshleyThomas87 <77860087+AshleyThomas87@users.noreply.github.com>
2021-09-10 15:08:40 -0700

    login mockData and route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,15 +1,28 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.route('/')
-    .get((req, res) => res.json({ message: 'server connection active' }))
+router
+  .route("/")
+  .get((req, res) => res.json({ message: "server connection active" }));
 
+router
+  .route("/test")
+  .get((req, res) => res.json({ message: "test get response successful" }))
+  .post((req, res) =>
+    res.json({
+      message: "test post response successful",
+      body: req.body,
+    })
+  );
 
-router.route('/test')
-    .get((req, res) => res.json({ message: 'test get response successful' }))
-    .post((req, res) => res.json({
-        message: 'test post response successful',
-        body: req.body
-    }))
-
+router.route("/test/login").post((req, res) => {
+    const data = require("./mockData/mockData.json");
+    const parsedData = JSON.parse(data);
+    const user = parsedData.filter((user) => user.email === req.body.email && user.password === req.body.password);
+    return user[0] ? res.json({
+        message: "response successful",
+        body: req.body,
+      })
+    : "User doesn't exsist";
+});
 
 module.exports = router;
