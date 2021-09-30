1c9f163e8a4a21ab95782bbcef432e634e40de52
Joshua-Beets <86432668+Joshua-Beets@users.noreply.github.com>
2021-09-30 10:12:56 -0700

    Update index.js

--- routes/api/user/index.js
+++ routes/api/user/index.js
@@ -1,43 +1,33 @@
 const router = require("express").Router();
 const UserModel = require("../../../DB/User");
 
-
 router.route("/user").post(async (req, res) => {
-
-
-    const { 
+  const { name, avatar, email, auth0Id } = req.body;
+  if (!name) {
+    return res.json({ error: true, message: "name field can not be empty" });
+  } else {
+    const newUserTest = new UserModel({
       name,
-     avatar,
+      avatar,
       email,
-      auth0Id
-    } = req.body;
-    if (!name) {
-      return res.json({ error: true, message: "name field can not be empty" });
-    } else {
-   const newUserTest = new UserModel({ 
-     name: name,
-    avatar:avatar,
-     email: email, 
-     auth0Id: auth0Id
+      auth0Id,
     });
-  
-    console.log(newUserTest, "test")
-      try {
-        const newUser = await newUserTest.save();
-        res.status(201).json(newUser);
-      } catch (err) {
-        res.status(400).json({ message: err.message });
-      }
-    }
-  });
-  
 
-  router.route("/all").get( async (req,res) =>{
-    const allUsers = await UserModel.find()
-    return res.json({
-      users: allUsers
-    })
+    console.log(newUserTest, "test");
+    try {
+      const newUser = await newUserTest.save();
+      res.status(201).json(newUser);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  }
+});
+
+router.route("/all").get(async (req, res) => {
+  const allUsers = await UserModel.find();
+  return res.json({
+    users: allUsers,
   });
-  
+});
 
 module.exports = router;
